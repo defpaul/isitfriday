2d0d58fe8bf24cfb94a37bfd98ac92a25b8eb510
devpaul <paul_Obercheker@gmx.de>
2024-05-03 21:37:37 +0200

    add the more feture

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
-use chrono::{self, Datelike, Weekday};
+use std::env;
+
+use chrono::{self, offset, Datelike, Local, Weekday};
 
 fn main() {
+    let args: Vec<String> = env::args().collect();
     let day = chrono::offset::Local::now().date().weekday();
     let fri = Weekday::Fri;
     if day == fri {
@@ -8,5 +11,10 @@ fn main() {
     }
     else {
         println!("Nope");
+    }
+
+    if &args[1] == "m" || &args[1] == "more" {
+        println!("{:?}", offset::Local::now());
+        println!("{:?}", offset::Utc::now());
     }
 }
