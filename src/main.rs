dd8ba5ceeb9723c96384430682d5c471c21367e2
devpaul <paul_Obercheker@gmx.de>
2024-05-04 10:01:18 +0200

    add day feture

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use std::env;
 
-use chrono::{self, offset, Datelike, Local, Weekday};
+use chrono::{self, offset, Datelike, Weekday};
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -18,10 +18,15 @@ fn main() {
 fn match_args(args: &[String]) {
     let arg1: String = String::from("-m");
     let arg2: String = String::from("--more");
+    let arg3: String = String::from("-d");
+    let arg4: String = String::from("--day");
     for i in args {
         if *i == arg1 || *i == arg2 {
             print_time(); 
         } 
+        else if *i == arg3 || *i == arg4{
+            println!("\nDay:\n{:?}", offset::Local::now().date().weekday());
+        }
    } 
 }
 
@@ -31,17 +36,18 @@ fn print_time() {
 }
 
 fn nop() {
-    print!("\n \x1b[38;5;50m
+    print!("\n \x1b[38;5;200m
      _______   ___     ______         _________
     /  _   |  /  /    /  __  \\       /  ___   / 
    /  / |  | /  /    /  /  \\  \\     /  /__/  / 
   /  /  |  |/  /    |  |    |  |   /  ______/ 
  /  /   |     /      \\  \\__/  /   /  / 
-/__/    |____/        \\______/   /__/  ");
+/__/    |____/        \\______/   /__/  
+");
 }
 
 fn yup() {
-    print!(" \x1b[38;5;200m
+    print!(" \x1b[38;5;50m
 ___      ___   ___      ___    ________
 \\  \\    /  /  |   |    |   |  |   __   |
  \\  \\  /  /   |   |    |   |  |  |__|  |
