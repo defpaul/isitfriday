43bf4af83b9ee6f38c2147538aa0ebf62e7e6263
devpaul <paul_Obercheker@gmx.de>
2025-05-17 18:54:05 +0200

    add weekend

--- src/main.rs
+++ src/main.rs
@@ -6,8 +6,13 @@ fn main() {
     let args: Vec<String> = env::args().collect();
     let day = chrono::offset::Local::now().date().weekday();
     let fri = Weekday::Fri;
+    let set = Weekday::Sat;
+    let sun = Weekday::Sun;
     if day == fri {
        yup();
+    }
+    else if day == set || day == sun{
+        weekend();
     }
     else {
         nop();
@@ -20,6 +25,8 @@ fn match_args(args: &[String]) {
     let arg_m1: String = String::from("--more");
     let arg_d0: String = String::from("-d");
     let arg_d1: String = String::from("--day");
+    let arg_h0: String = String::from("-h");
+    let arg_h1: String = String::from("--help");
     for i in args {
         if *i == arg_m0 || *i == arg_m1 {
             print_time(); 
@@ -27,12 +34,25 @@ fn match_args(args: &[String]) {
         else if *i == arg_d0 || *i == arg_d1{
             println!("\nDay:\n{:?}", offset::Local::now().date().weekday());
         }
+        else if *i == arg_h0 || *i == arg_h1{
+            help();
+        }
+
    } 
 }
 
 fn print_time() { 
     println!("\nLockal:\n{:?}", offset::Local::now());
     println!("\nUtc:\n{:?}", offset::Utc::now());
+}
+
+fn help(){
+    print!(r#"
+        This is the help text to this programm.
+            -m (--more) displais time
+            -d (--day) displayes the day
+            -h (--help) displais this masage
+"#)
 }
 
 fn nop() {
@@ -48,6 +68,23 @@ fn nop() {
     colore("clear");
 }
 
+fn weekend(){
+    colore("gold");
+    print!(r#"
+
+  _ _   _             _                    _       
+ (_) |_( )___    __ _| |_ __ ___  __ _  __| |_   _ 
+ | | __|// __|  / _` | | '__/ _ \/ _` |/ _` | | | |
+ | | |_  \__ \ | (_| | | | |  __/ (_| | (_| | |_| |
+ |_|\__| |___/  \__,_|_|_|  \___|\__,_|\__,_|\__, |
+ __      _____  ___| | _____ _ __   __| |    |___/ 
+ \ \ /\ / / _ \/ _ \ |/ / _ \ '_ \ / _` |          
+  \ V  V /  __/  __/   <  __/ | | | (_| |          
+   \_/\_/ \___|\___|_|\_\___|_| |_|\__,_|          
+"#);
+    colore("clear");
+}
+
 fn yup() {
     colore("grean");
     print!(r#"
@@ -55,7 +92,7 @@ ___      ___   ___      ___    ________
 \  \    /  /  |   |    |   |  |   __   |
  \  \  /  /   |   |    |   |  |  |__|  |
   \  \/  /    |   |    |   |  |  ______|
-   \    /     |    \___/   |  |  | 
+   \    /     |    \__/    |  |  | 
     /  /       \          /   |  |
    /__/         \________/    |__|
 "#);
@@ -68,6 +105,7 @@ fn colore(color: &str) {
        "red" => print!("\x1b[38;5;196m"),
         "grean" => print!("\x1b[38;5;82m"), 
         "clear" => print!("\x1b[0m"),
+        "gold" => print!("\x1b[38;5;46m"),
        _ => ()
    }
 }
